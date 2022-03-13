1cc80c632cccef28c5684f59de7f341f986a1d7b
John Requizo <jereq1@student.monash.edu>
2022-03-13 21:54:40 +1100

    Initial commit

---
+++ game-of-life/draw.js
@@ -0,0 +1,9 @@
+main();
+
+
+function main() {
+    let canvas = document.getElementById("board");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 150, 75);
+}
