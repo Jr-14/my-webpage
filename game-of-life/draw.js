d307991900141a9b502bb483c49581f807a700ef
John Requizo <jereq1@student.monash.edu>
2022-03-13 22:35:20 +1100

    Add logging of board coordinates

--- game-of-life/draw.js
+++ game-of-life/draw.js
@@ -6,4 +6,22 @@ function main() {
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(0, 0, 150, 75);
+
+    // Log events
+    canvas.addEventListener("mousemove", (e) => {
+        logCanvasCoordinates(e);
+    });
+}
+
+
+/** Log the canvas coordinates into the console
+ *
+ */
+function logCanvasCoordinates(event) {
+    let x = event.offsetX;
+    let y = event.offsetY;
+    let p = document.getElementById("board-coordinates");
+    let log = "Coordinates (x: " + x + ", y: " + y + ")";
+    // console.log(log);
+    p.innerHTML = log;
 }
