11dde739e048ca001098d0e4ad8ab183bf60ca04
John Requizo <jereq1@student.monash.edu>
2022-03-14 00:40:07 +1100

    Add border and gameboard size to GameWorld

--- game-of-life/gameworld.js
+++ game-of-life/gameworld.js
@@ -3,14 +3,17 @@ class GameWorld {
     /** Creates a new game world and places a gameboard within it
      * 
      */
-    constructor() {
-        let createCanvasElement = () => {
+    constructor(height, width) {
+        let createCanvasElement = (w, h) => {
             let element = document.createElement("canvas");
             element.id = "board";
+            element.width = w;
+            element.height = h;
+            element.style.border = "1px solid #000000";
             return element;
         };
 
-        this.context = createCanvasElement();
+        this.context = createCanvasElement(height, width);
         this.numberOfRows = this.context.height / 10;
         this.numberOfColumns = this.context.width / 10;
     }
